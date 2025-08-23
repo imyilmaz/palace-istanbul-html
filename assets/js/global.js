56801667fb06f7390c1e61a50660cc7397665d04
imyilmaz <imyilmaz53@gmail.com>
2025-08-23 16:09:32 +0300

    palace-istanbul-html

---
+++ assets/js/global.js
@@ -0,0 +1,24 @@
+AOS.init({
+    once: false,  // Animasyonlar sadece bir kez tetiklenecek
+    mobile: false, // Mobil cihazlarda AOS'u devre dışı bırakır
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector('.hamburger');
+    const header = document.querySelector('.header');
+    const menuOverlay = document.querySelector('.menu-overlay');
+
+    // Hamburger menüsüne tıklama
+    hamburger.addEventListener('click', function() {
+        header.classList.toggle('open'); // open sınıfını ekle veya kaldır
+        hamburger.classList.toggle('open'); // open sınıfını ekle veya kaldır
+    });
+
+    // Menü overlay'ine tıklama
+    if (menuOverlay) {
+        menuOverlay.addEventListener('click', function() {
+            header.classList.remove('open'); // open sınıfını kaldır
+            hamburger.classList.remove('open'); // open sınıfını kaldır
+        });
+    }
+});
