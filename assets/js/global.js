86cf76613ca21c3805c917e4a89cd8e4f8e7dbf3
imyilmaz <imyilmaz53@gmail.com>
2025-08-24 17:08:31 +0300

    revizeler

--- assets/js/global.js
+++ assets/js/global.js
@@ -7,11 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector('.hamburger');
     const header = document.querySelector('.header');
     const menuOverlay = document.querySelector('.menu-overlay');
+    const body = document.querySelector('body');
 
     // Hamburger menüsüne tıklama
     hamburger.addEventListener('click', function() {
         header.classList.toggle('open'); // open sınıfını ekle veya kaldır
         hamburger.classList.toggle('open'); // open sınıfını ekle veya kaldır
+        body.classList.toggle('hidden'); // hidden sınıfını ekle veya kaldır
     });
 
     // Menü overlay'ine tıklama
